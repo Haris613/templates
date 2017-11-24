92f79ad9f5845f8ceccb8a805d120fa985cf67a9
Haris <rostislav.osvald@gmail.com>
2017-11-24 22:17:01 +0100

    VECTOR important getters + operator[]

--- myvector.h
+++ myvector.h
@@ -14,6 +14,14 @@ public:
 	~myVector(){
 		delete m_array;
 	}
+	MViterator begin(){ return m_array; }
+	MViterator end(){ return m_array+m_size; }//todo maybe not right
+	size_t size(){ return m_size; }
+	size_t capacity(){ return m_capacity; }
+	bool empty(){ return m_size == 0; }
+	T & operator[](size_t n){
+		return m_array[n];
+	}
 private:
 	T * m_array;
 	size_t m_size;
