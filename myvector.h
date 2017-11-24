77582ca953aa83f7fa391e07bea6017984d23d0a
Haris <rostislav.osvald@gmail.com>
2017-11-24 22:15:14 +0100

    VECTOR basic structure

--- myvector.h
+++ myvector.h
@@ -1,6 +1,24 @@
 #ifndef _MYVECTOR_H_
 #define _MYVECTOR_H_
 
+#include <cstddef>
+
+using namespace std;
+
+template <class T>
+class myVector
+{
+	typedef T* MViterator;
+public:
+	myVector():m_size(0), m_capacity(1){}
+	~myVector(){
+		delete m_array;
+	}
+private:
+	T * m_array;
+	size_t m_size;
+	size_t m_capacity;
+};
 
 
 #endif
